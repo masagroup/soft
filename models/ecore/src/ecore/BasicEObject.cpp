8014f124aeb755b51a64d867a72fdddd39ebc9fc
Jocelyn Dumay <jdy@masagroup.net>
2018-10-19 14:07:59 +0200

    [ecore] implements BasicEObject getter setter methods

--- models/ecore/src/ecore/BasicEObject.cpp
+++ models/ecore/src/ecore/BasicEObject.cpp
@@ -1,12 +1,20 @@
 #include "BasicEObject.hpp"
 #include "ecore/BasicEList.hpp"
 #include "ecore/EAdapter.hpp"
+#include "ecore/EClass.hpp"
+#include "ecore/EStructuralFeature.hpp"
+
+#include <boost/assert.hpp>
+#include <string>
+#include <sstream>
 
 using namespace ecore;
 
 BasicEObject::BasicEObject()
     : eDeliver_(false)
     , eAdapters_( new BasicEList<EAdapter*>() )
+    , eContainer_()
+    , eContainingFeature_()
 {
 
 }
@@ -15,6 +23,7 @@ BasicEObject::~BasicEObject()
 {
 
 }
+
 
 EList<EAdapter*>& BasicEObject::eAdapters()
 {
@@ -41,3 +50,99 @@ bool BasicEObject::eNotificationRequired()
 {
     return eDeliver_ && eAdapters_->size() > 0;
 }
+
+boost::any ecore::BasicEObject::eGet( const std::shared_ptr<ecore::EStructuralFeature>& feature ) const
+{
+    return eGet(feature,true);
+}
+
+boost::any ecore::BasicEObject::eGet( const std::shared_ptr<ecore::EStructuralFeature>& feature, bool resolve ) const
+{
+    return eGet( feature, true );
+}
+
+int ecore::BasicEObject::eDerivedStructuralFeatureID( const std::shared_ptr<EStructuralFeature>& eStructuralFeature ) const
+{
+    BOOST_VERIFY_MSG( eClass()->getEAllStructuralFeatures()->contains( eStructuralFeature ), ( static_cast<std::ostringstream&>( std::stringstream() << "The feature '" << eStructuralFeature->getName() << "' is not a valid feature" )).str().c_str() );
+    return eStructuralFeature->getFeatureID();
+}
+
+boost::any ecore::BasicEObject::eGet( const std::shared_ptr<ecore::EStructuralFeature>& eFeature, bool resolve, bool coreType ) const
+{
+    int featureID = eDerivedStructuralFeatureID( eFeature );
+    if( featureID >= 0 )
+        return eGet( featureID, resolve, coreType );
+    throw "The feature '" + eFeature->getName() + "' is not a valid feature";
+}
+
+boost::any ecore::BasicEObject::eGet( int featureID, bool resolve, bool coreType ) const
+{
+    std::shared_ptr<EStructuralFeature> eFeature = eClass()->getEStructuralFeature( featureID );
+    BOOST_ASSERT_MSG( eFeature != nullptr, ( static_cast<std::ostringstream&>( std::stringstream() << "Invalid featureID:  " << featureID )).str().c_str() );
+    return boost::any();
+}
+
+bool ecore::BasicEObject::eIsSet( const std::shared_ptr<ecore::EStructuralFeature>& eFeature ) const
+{
+    int featureID = eDerivedStructuralFeatureID( eFeature );
+    if( featureID >= 0 )
+        return eIsSet( featureID );
+    throw "The feature '" + eFeature->getName() + "' is not a valid feature";
+}
+
+bool ecore::BasicEObject::eIsSet( int featureID ) const
+{
+    std::shared_ptr<EStructuralFeature> eFeature = eClass()->getEStructuralFeature( featureID );
+    BOOST_ASSERT_MSG( eFeature != nullptr, ( static_cast<std::ostringstream&>( std::stringstream() << "Invalid featureID:  " << featureID ) ).str().c_str() );
+    return false;
+}
+
+void ecore::BasicEObject::eSet( const std::shared_ptr<ecore::EStructuralFeature>& eFeature, const boost::any & newValue )
+{
+    int featureID = eDerivedStructuralFeatureID( eFeature );
+    if( featureID >= 0 )
+        eSet( featureID , newValue );
+    throw "The feature '" + eFeature->getName() + "' is not a valid feature";
+}
+
+bool ecore::BasicEObject::eSet( int featureID, const boost::any & newValue )
+{
+    std::shared_ptr<EStructuralFeature> eFeature = eClass()->getEStructuralFeature( featureID );
+    BOOST_ASSERT_MSG( eFeature != nullptr, ( static_cast<std::ostringstream&>( std::stringstream() << "Invalid featureID:  " << featureID ) ).str().c_str() );
+    return false;
+}
+
+
+void ecore::BasicEObject::eUnset( const std::shared_ptr<ecore::EStructuralFeature>& eFeature )
+{
+    int featureID = eDerivedStructuralFeatureID( eFeature );
+    if( featureID >= 0 )
+        eUnset( featureID );
+    throw "The feature '" + eFeature->getName() + "' is not a valid feature";
+}
+
+void ecore::BasicEObject::eUnset( int featureID )
+{
+    std::shared_ptr<EStructuralFeature> eFeature = eClass()->getEStructuralFeature( featureID );
+    BOOST_ASSERT_MSG( eFeature != nullptr, ( static_cast<std::ostringstream&>( std::stringstream() << "Invalid featureID:  " << featureID ) ).str().c_str() );
+}
+
+void ecore::BasicEObject::eInverseAdd( int featureID, const boost::any & newValue )
+{
+}
+
+void ecore::BasicEObject::eInverseRemove( int featureID, const boost::any & oldValue )
+{
+}
+
+void ecore::BasicEObject::setEContainer( const std::shared_ptr<EObject>& eContainer, const std::shared_ptr<EStructuralFeature>& eContainingFeature )
+{
+}
+
+
+
+
+
+
+
+
--- models/ecore/src/ecore/BasicEObject.hpp
+++ models/ecore/src/ecore/BasicEObject.hpp
@@ -21,19 +21,40 @@ namespace ecore
 
         virtual ~BasicEObject();
 
+        // Notification
         virtual EList<EAdapter*>& eAdapters();
-
         virtual bool eDeliver();
-
         virtual void eSetDeliver( bool deliver );
-
         virtual void eNotify( const ENotification& notification );
-
         bool eNotificationRequired();
+
+        // Getter/ Setter
+        virtual boost::any eGet( const std::shared_ptr<ecore::EStructuralFeature>& feature ) const;
+        virtual boost::any eGet( const std::shared_ptr<ecore::EStructuralFeature>& feature, bool resolve ) const;
+        virtual boost::any eGet( const std::shared_ptr<ecore::EStructuralFeature>& feature, bool resolve , bool coreType) const;
+        virtual bool eIsSet( const std::shared_ptr<ecore::EStructuralFeature>& feature ) const;
+        virtual void eSet( const std::shared_ptr<ecore::EStructuralFeature>& feature, const boost::any& newValue );
+        virtual void eUnset( const std::shared_ptr<ecore::EStructuralFeature>& feature );
+
+        // Container
+        virtual void eInverseAdd( int featureID, const boost::any& newValue );
+        virtual void eInverseRemove( int featureID, const boost::any& oldValue );
+        void setEContainer( const std::shared_ptr<EObject>& eContainer, const std::shared_ptr<EStructuralFeature>& eContainingFeature );
+
+    protected:
+        virtual int eDerivedStructuralFeatureID( const std::shared_ptr<EStructuralFeature>& eStructuralFeature ) const;
+
+
+        virtual boost::any eGet(int featureID, bool resolve, bool coreType) const;
+        virtual bool eIsSet(int featureID) const;
+        virtual bool eSet(int featureID, const boost::any& newValue);
+        virtual void eUnset( int featureID );
 
     protected:
         std::shared_ptr< EList<EAdapter*> > eAdapters_;
         bool eDeliver_;
+        std::weak_ptr<EObject> eContainer_;
+        std::weak_ptr<EStructuralFeature> eContainingFeature_;
     };
 
 }
