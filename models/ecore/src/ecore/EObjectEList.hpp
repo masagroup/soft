193b9ab1b558704675c621d3f0ba20bd757a14c9
Jocelyn Dumay <jdy@masagroup.net>
2018-10-29 16:57:27 +0100

    [ecore] handles notification chain in EObjectEList

--- models/ecore/src/ecore/EObjectEList.hpp
+++ models/ecore/src/ecore/EObjectEList.hpp
@@ -32,11 +32,11 @@ namespace ecore
             , inverseFeatureID_( -1 )
             , inverse_( *this )
         {
-            std::shared_ptr<EReference> reference = std::dynamic_pointer_cast<EReference>(owner->eClass()->getEStructuralFeature( featureID ));
-            if (reference)
+            std::shared_ptr<EReference> reference = std::dynamic_pointer_cast<EReference>( owner->eClass()->getEStructuralFeature( featureID ) );
+            if( reference )
             {
                 std::shared_ptr<EReference> opposite = reference->getEOpposite();
-                if (opposite)
+                if( opposite )
                     inverseFeatureID_ = opposite->getFeatureID();
             }
         }
@@ -56,49 +56,47 @@ namespace ecore
 
         virtual void addUnique( const T& e )
         {
-            auto object = std::dynamic_pointer_cast<BasicEObject>(e);
-            if (object)
-            {
-                std::size_t index = size();
-                BasicEList<T>::addUnique( e );
-                inverse_.inverseAdd( object );
-                if (isNotificationRequired())
-                    dispatchNotification( createNotification( ENotification::ADD, nullptr, e, index ) );
-            }
+            auto index = size();
+            BasicEList<T>::addUnique( e );
+            auto object = std::dynamic_pointer_cast<BasicEObject>( e );
+            _ASSERTE( object );
+            auto notifications = inverse_.inverseAdd( object, nullptr );
+            createAndDispatchNotification( notifications, ENotification::ADD, nullptr, e, index );
         }
 
         virtual void addUnique( std::size_t index, const T& e )
         {
-            auto object = std::dynamic_pointer_cast<BasicEObject>(e);
-            if (object)
-            {
-                BasicEList<T>::addUnique( index, e );
-                inverse_.inverseAdd( object );
-                if (isNotificationRequired())
-                    dispatchNotification( createNotification( ENotification::ADD, nullptr, e, index ) );
-            }
+            BasicEList<T>::addUnique( index, e );
+            auto object = std::dynamic_pointer_cast<BasicEObject>( e );
+            _ASSERTE( object );
+            auto notifications = inverse_.inverseAdd( object, nullptr );
+            createAndDispatchNotification( notifications, ENotification::ADD, nullptr, e, index );
         }
 
-        virtual T remove( std::size_t index ) {
+        virtual T remove( std::size_t index )
+        {
             auto oldObject = BasicEList<T>::remove( index );
-            if (auto oldBasicObject = std::dynamic_pointer_cast<BasicEObject>(oldObject))
-                inverse_.inverseRemove( oldBasicObject );
-            if (isNotificationRequired())
-                dispatchNotification( createNotification( ENotification::REMOVE, oldObject, nullptr, index ) );
+            auto oldBasicObject = std::dynamic_pointer_cast<BasicEObject>( oldObject );
+            _ASSERTE( oldBasicObject );
+            auto notifications = inverse_.inverseRemove( oldBasicObject, nullptr );
+            createAndDispatchNotification( notifications, ENotification::REMOVE, oldObject, nullptr, index );
             return oldObject;
         }
 
-        virtual T setUnique( std::size_t index, const T& newObject ) {
+        virtual T setUnique( std::size_t index, const T& newObject )
+        {
             T oldObject = BasicEList<T>::setUnique( index, newObject );
-            if (newObject != oldObject)
+            if( newObject != oldObject )
             {
-                if (auto oldBasicObject = std::dynamic_pointer_cast<BasicEObject>(oldObject))
-                    inverse_.inverseRemove( oldBasicObject );
-                if (auto newBasicObject = std::dynamic_pointer_cast<BasicEObject>(newObject))
-                    inverse_.inverseAdd( newBasicObject );
+                auto oldBasicObject = std::dynamic_pointer_cast<BasicEObject>( oldObject );
+                auto newBasicObject = std::dynamic_pointer_cast<BasicEObject>( newObject );
+                _ASSERTE( oldBasicObject );
+                _ASSERTE( newBasicObject );
+                std::shared_ptr<ENotificationChain> notifications;
+                notifications = inverse_.inverseRemove( oldBasicObject, notifications );
+                notifications = inverse_.inverseAdd( newBasicObject, notifications );
+                createAndDispatchNotification( notifications , ENotification::SET, oldObject, newObject, index );
             }
-            if (isNotificationRequired())
-                dispatchNotification( createNotification( ENotification::SET, oldObject, newObject, index ) );
             return oldObject;
         }
 
@@ -112,11 +110,19 @@ namespace ecore
         template <typename Q, bool opposite = false >
         struct Opposite
         {
-            Opposite( EObjectEList& list ) : list_( list ) {}
+            Opposite( EObjectEList& list ): list_( list )
+            {
+            }
 
-            inline void inverseAdd( const Q& q ) { q->eInverseAdd( list_.getOwner(), BasicEObject::EOPPOSITE_FEATURE_BASE - list_.featureID_ ); }
+            inline std::shared_ptr<ENotificationChain> inverseAdd( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return q->eInverseAdd( list_.getOwner(), BasicEObject::EOPPOSITE_FEATURE_BASE - list_.featureID_ , notifications );
+            }
 
-            inline void inverseRemove( const Q& q ) { q->eInverseRemove( list_.getOwner(), BasicEObject::EOPPOSITE_FEATURE_BASE - list_.featureID_ ); }
+            inline std::shared_ptr<ENotificationChain> inverseRemove( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return q->eInverseRemove( list_.getOwner(), BasicEObject::EOPPOSITE_FEATURE_BASE - list_.featureID_ , notifications );
+            }
 
             EObjectEList& list_;
         };
@@ -124,11 +130,19 @@ namespace ecore
         template <typename Q>
         struct Opposite<Q, true>
         {
-            Opposite( EObjectEList& list ) : list_( list ) {}
+            Opposite( EObjectEList& list ): list_( list )
+            {
+            }
 
-            inline void inverseAdd( const Q& q ) { q->eInverseAdd( list_.getOwner(), list_.inverseFeatureID_ ); }
+            inline std::shared_ptr<ENotificationChain> inverseAdd( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return q->eInverseAdd( list_.getOwner(), list_.inverseFeatureID_ , notifications );
+            }
 
-            inline void inverseRemove( const Q& q ) { q->eInverseRemove( list_.getOwner(), list_.inverseFeatureID_ ); }
+            inline std::shared_ptr<ENotificationChain> inverseRemove( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return q->eInverseRemove( list_.getOwner(), list_.inverseFeatureID_ , notifications );
+            }
 
             EObjectEList& list_;
         };
@@ -136,32 +150,50 @@ namespace ecore
         template <typename Q, bool inverse = false, bool opposite = false >
         struct Inverse
         {
-            inline Inverse( EObjectEList& list ) {}
+            inline Inverse( EObjectEList& list )
+            {
+            }
 
-            inline void inverseAdd( const Q& q ) {}
+            inline std::shared_ptr<ENotificationChain> inverseAdd( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return  notifications;
+            }
 
-            inline void inverseRemove( const Q& q ) {}
+            inline std::shared_ptr<ENotificationChain> inverseRemove( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return  notifications;
+            }
         };
 
         template <typename Q, bool opposite>
         struct Inverse<Q, true, opposite>
         {
-            inline Inverse( EObjectEList& list ) : opposite_( list ) {}
+            inline Inverse( EObjectEList& list ): opposite_( list )
+            {
+            }
 
-            inline void inverseAdd( const Q& q ) { opposite_.inverseAdd( q ); }
+            inline std::shared_ptr<ENotificationChain> inverseAdd( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return opposite_.inverseAdd( q , notifications );
+            }
 
-            inline void inverseRemove( const Q& q ) { opposite_.inverseRemove( q ); }
+            inline std::shared_ptr<ENotificationChain> inverseRemove( const Q& q, const std::shared_ptr<ENotificationChain>& notifications )
+            {
+                return opposite_.inverseRemove( q , notifications );
+            }
 
             Opposite<Q, opposite> opposite_;
         };
 
     private:
 
-        std::shared_ptr<BasicEObject> getOwner() {
+        std::shared_ptr<BasicEObject> getOwner()
+        {
             return owner_.lock();
         }
 
-        bool isNotificationRequired() const {
+        bool isNotificationRequired() const
+        {
             auto owner = owner_.lock();
             return owner ? owner->eNotificationRequired() : false;
         }
@@ -172,10 +204,24 @@ namespace ecore
             return owner ? std::make_shared<Notification>( eventType, owner, owner->eClass()->getEStructuralFeature( featureID_ ), oldValue, newValue, position ) : nullptr;
         }
 
-        void dispatchNotification( const std::shared_ptr<ENotification>& notification ) const
+        void createAndDispatchNotification( const std::shared_ptr<ENotificationChain>& notifications, ENotification::EventType eventType, const T& oldValue, const T& newValue, std::size_t position ) const
         {
-            if (auto owner = owner_.lock())
-                owner->eNotify( notification );
+            if( isNotificationRequired() )
+            {
+                auto notification = createNotification( eventType, oldValue, newValue, position );
+                if( notifications )
+                {
+                    notifications->add( notification );
+                    notifications->dispatch();
+                }
+                if( auto owner = owner_.lock() )
+                    owner->eNotify( notification );
+            }
+            else
+            {
+                if( notifications )
+                    notifications->dispatch();
+            }
         }
 
 
