56b0de81a7f109e3a06f1f6732abe2a668311a38
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2018-11-01 12:25:22 +0100

    [tests] use notifier and feature mockups to test notification constructor

--- tests/ecore.tests/src/NotificationTests.cpp
+++ tests/ecore.tests/src/NotificationTests.cpp
@@ -10,20 +10,22 @@ BOOST_AUTO_TEST_SUITE( NotificationTests )
 
 BOOST_AUTO_TEST_CASE( Constructor )
 {
+    std::shared_ptr<MockNotifier> notifier = std::make_shared<MockNotifier>();
+    std::shared_ptr<MockStructuralFeature> feature = std::make_shared<MockStructuralFeature>();
     {
-        auto notification = std::make_shared<Notification>( ENotification::ADD, nullptr, nullptr, 1, 2 );
+        auto notification = std::make_shared<Notification>( ENotification::ADD, notifier, feature, 1, 2 );
         BOOST_CHECK_EQUAL( notification->getEventType(), ENotification::ADD );
-        //BOOST_CHECK_EQUAL( notification->getNotifier(), nullptr );
-        //BOOST_CHECK_EQUAL( notification->getFeature(), nullptr );
+        BOOST_CHECK_EQUAL( notification->getNotifier(), notifier );
+        BOOST_CHECK_EQUAL( notification->getFeature(), feature );
         BOOST_CHECK_EQUAL( boost::any_cast<int>(notification->getOldValue()), 1 );
         BOOST_CHECK_EQUAL( boost::any_cast<int>( notification->getNewValue()), 2 );
         BOOST_CHECK_EQUAL( notification->getPosition(), -1 );
     }
     {
-        auto notification = std::make_shared<Notification>( ENotification::ADD, nullptr, nullptr, 1, 2 , 10 );
+        auto notification = std::make_shared<Notification>( ENotification::ADD, notifier, feature, 1, 2 , 10 );
         BOOST_CHECK_EQUAL( notification->getEventType(), ENotification::ADD );
-        //BOOST_CHECK_EQUAL( notification->getNotifier(), nullptr );
-        //BOOST_CHECK_EQUAL( notification->getFeature(), nullptr );
+        BOOST_CHECK_EQUAL( notification->getNotifier(), notifier );
+        BOOST_CHECK_EQUAL( notification->getFeature(), feature );
         BOOST_CHECK_EQUAL( boost::any_cast<int>( notification->getOldValue() ), 1 );
         BOOST_CHECK_EQUAL( boost::any_cast<int>( notification->getNewValue() ), 2 );
         BOOST_CHECK_EQUAL( notification->getPosition(), 10 );
