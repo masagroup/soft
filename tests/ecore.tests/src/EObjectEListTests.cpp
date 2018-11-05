85ef3a4a32239396290643aa2c0290a0dde2ef7e
Jocelyn Dumay <jdy@masagroup.net>
2018-11-05 16:34:42 +0100

    [tests] test EObjectEList add no notifications

--- tests/ecore.tests/src/EObjectEListTests.cpp
+++ tests/ecore.tests/src/EObjectEListTests.cpp
@@ -22,4 +22,24 @@ BOOST_AUTO_TEST_CASE( Constructor )
     }
 }
 
+BOOST_AUTO_TEST_CASE( Add )
+{
+    auto object = std::make_shared<MockObject>();
+    auto owner = std::make_shared<MockObject>();
+    {
+        std::shared_ptr<EList<std::shared_ptr<EObject>>> list = std::make_shared<EObjectEList<std::shared_ptr<EObject>>>( owner, 1, 2 );
+        MOCK_EXPECT( owner->eDeliver ).once().returns( false );
+        BOOST_CHECK( list->add( object ) );
+        BOOST_CHECK_EQUAL( list->size() , 1 );
+        BOOST_CHECK_EQUAL( list->get( 0 ), object );
+        BOOST_CHECK( !list->add( object ) );
+    }
+    {
+        std::shared_ptr<EList<std::shared_ptr<EObject>>> list = std::make_shared<EObjectEList<std::shared_ptr<EObject>>>( owner, 1, 2 );
+        //MOCK_EXPECT( owner->eDeliver ).once().returns( true );
+        //list->add( object );
+    }
+    
+}
+
 BOOST_AUTO_TEST_SUITE_END()
