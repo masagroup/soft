88060ec0fcfdbbe76fc683471f88a011006b3a8d
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2018-11-11 09:49:19 +0100

    [tests] add a test for performance checking between Diamon Architecture and Mixin one

---
+++ tests/ecore.tests/src/DiamondVsMixinsTests.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/execution_monitor.hpp>
+#include <boost/timer/timer.hpp>
+
+using namespace boost::timer;
+
+BOOST_AUTO_TEST_SUITE( DiamondVsMixinsTests )
+
+BOOST_AUTO_TEST_CASE( Performance )
+{
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END()
