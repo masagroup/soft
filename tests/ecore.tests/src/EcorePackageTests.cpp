a84267581ca5f40f77b8d55d98c6e8702a92736d
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2018-11-21 19:29:24 +0100

    [tests] add EcorePackage tests

---
+++ tests/ecore.tests/src/EcorePackageTests.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/auto_unit_test.hpp>
+
+#include "ecore/EcorePackage.hpp"
+
+using namespace ecore;
+
+BOOST_AUTO_TEST_SUITE(EcorePackageTests)
+
+BOOST_AUTO_TEST_CASE(Constructor)
+{
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END()
