8200b07123f14188e9d50325d9124c81404c6cf5
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2018-11-28 18:38:10 +0100

    [ecore] remove parameter in EObjectEList constructor

--- libraries/ecore/src/ecore/impl/EObjectEList.hpp
+++ libraries/ecore/src/ecore/impl/EObjectEList.hpp
@@ -32,13 +32,13 @@ namespace ecore::impl
         using EList<T>::add;
         using ENotifyingList<T>::add;
 
-        template <typename U = T, long = null_v<std::enable_if_t< !proxies >>::value >
+        template <long = null_v<std::enable_if_t< !proxies >>::value >
         EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID = -1 )
             : AbstractEObjectEList( owner, featureID, inverseFeatureID )
         {
         }
 
-        template <typename U = T, int = null_v<std::enable_if_t< proxies >>::value >
+        template <int = null_v<std::enable_if_t< proxies >>::value >
         EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID = -1 )
             : AbstractEObjectEList( []( const Proxy<T>& p ) { return p.get(); }
                                   , [=]( const T& v ) { return Proxy<T>(owner,featureID_,v); } 
