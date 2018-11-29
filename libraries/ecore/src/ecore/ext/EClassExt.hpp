9493df0a36307b3a54fa13e8cbc8589f57593849
Jocelyn Dumay <jdy@masagroup.net>
2018-11-29 15:37:20 +0100

    [ecore] remove user code from EClassImpl and move it to EClassExt class

--- libraries/ecore/src/ecore/ext/EClassExt.hpp
+++ libraries/ecore/src/ecore/ext/EClassExt.hpp
@@ -12,6 +12,8 @@
 
 #include "ecore/impl/EClassImpl.hpp"
 
+#include <unordered_map>
+
 namespace ecore::ext
 {
     class EClassExt : public impl::EClassImpl
@@ -25,6 +27,47 @@ namespace ecore::ext
     
     public:
         virtual ~EClassExt();
+
+        //*********************************
+        // Operations
+        //*********************************
+        virtual bool isSuperTypeOf( const std::shared_ptr<ecore::EClass>& someClass );
+        virtual int getFeatureCount();
+        virtual std::shared_ptr<ecore::EStructuralFeature> getEStructuralFeature( int featureID );
+        virtual std::shared_ptr<ecore::EStructuralFeature> getEStructuralFeature( const std::string& featureName );
+        virtual int getFeatureID( const std::shared_ptr<ecore::EStructuralFeature>& feature );
+        virtual int getOperationCount();
+        virtual std::shared_ptr<ecore::EOperation> getEOperation( int operationID );
+        virtual int getOperationID( const std::shared_ptr<ecore::EOperation>& operation );
+        virtual std::shared_ptr<ecore::EOperation> getOverride( const std::shared_ptr<ecore::EOperation>& operation );
+       
+        void setModified( int featureID );
+    
+    protected:
+        //*********************************
+        // Initializers
+        //*********************************
+        virtual void initEAllAttributes();
+        virtual void initEAllReferences();
+        virtual void initEReferences();
+        virtual void initEAttributes();
+        virtual void initEAllContainments();
+        virtual void initEAllCrossReferences();
+        virtual void initEAllOperations();
+        virtual void initEAllStructuralFeatures();
+        virtual void initEAllSuperTypes();
+        virtual void initEIDAttribute();
+   
+    private:
+        void initFeaturesSubSet();
+        void initNameToFeatureMap();
+        void initOperationToOverrideMap();
+
+    private:
+        class ESuperAdapter;
+        std::unique_ptr<ESuperAdapter> eSuperAdapter_;
+        std::unique_ptr< std::unordered_map< std::string, std::shared_ptr<EStructuralFeature>>> nameToFeatureMap_;
+        std::unique_ptr< std::unordered_map< std::shared_ptr<EOperation>, std::shared_ptr<EOperation>>> operationToOverrideMap_;
     };
 }
 #endif /* ECORE_ECLASS_ECLASSEXT_HPP */
