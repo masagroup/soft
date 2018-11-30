4a360561f3bfb1d96de31b12381b52c11ecbb998
Jocelyn Dumay <jdy@masagroup.net>
2018-11-30 10:34:24 +0100

    [ecore] add EAttributeExt as an EAttributeImpl extension

---
+++ libraries/ecore/src/ecore/ext/EAttributeExt.cpp
@@ -0,0 +1,13 @@
+#include "ecore/ext/EAttributeExt.hpp"
+
+using namespace ecore;
+using namespace ecore::ext;
+using namespace ecore::impl;
+
+EAttributeExt::EAttributeExt()
+{
+}
+
+EAttributeExt::~EAttributeExt()
+{
+}
---
+++ libraries/ecore/src/ecore/ext/EAttributeExt.hpp
@@ -0,0 +1,30 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2018 MASA Group
+//
+// *****************************************************************************
+
+#ifndef ECORE_EATTRIBUTE_EATTRIBUTEEXT_HPP
+#define ECORE_EATTRIBUTE_EATTRIBUTEEXT_HPP
+
+#include "ecore/impl/EAttributeImpl.hpp"
+
+namespace ecore::ext
+{
+    class EAttributeExt : public virtual impl::EAttributeImpl
+    {
+    private:
+        EAttributeExt& operator=( EAttributeExt const& ) = delete;
+
+    protected:
+        friend class impl::EcoreFactoryImpl;
+        EAttributeExt();
+
+    public:
+        virtual ~EAttributeExt();
+    };
+}
+#endif /* ECORE_EATTRIBUTE_EATTRIBUTEEXT_HPP */
