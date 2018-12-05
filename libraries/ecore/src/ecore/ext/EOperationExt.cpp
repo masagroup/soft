f2d819c2c1826df8948ab5ceba1036c54da576b6
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2018-12-05 16:33:56 +0100

    [ecore] add EOperationExt class as an EOperationImpl extension

---
+++ libraries/ecore/src/ecore/ext/EOperationExt.cpp
@@ -0,0 +1,13 @@
+#include "ecore/ext/EOperationExt.hpp"
+
+using namespace ecore;
+using namespace ecore::impl;
+using namespace ecore::ext;
+
+EOperationExt::EOperationExt()
+{
+}
+
+EOperationExt::~EOperationExt()
+{
+}
