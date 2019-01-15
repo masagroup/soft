460370fa305f970b69379e54288eb38f732c5060
Jocelyn Dumay <jdy@masagroup.net>
2019-01-15 11:02:11 +0100

    [tests] add Resource and ResourceSet tests files

---
+++ tests/ecore.tests/src/ResourceSetTests.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/execution_monitor.hpp>
+
+#include "ecore/impl/ResourceSet.hpp"
+
+using namespace ecore;
+using namespace ecore::impl;
+
+BOOST_AUTO_TEST_SUITE( ResourceSetTests )
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ tests/ecore.tests/src/ResourceTests.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/execution_monitor.hpp>
+
+#include "ecore/impl/Resource.hpp"
+
+using namespace ecore;
+
+BOOST_AUTO_TEST_SUITE( ResourceTests )
+
+BOOST_AUTO_TEST_SUITE_END()
