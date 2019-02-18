6684b3e2e6e21507d7c38b082e5d1d5778e37809
Jocelyn Dumay <jdy@masagroup.net>
2019-02-18 17:51:55 +0100

    [ecore] tests in simple book test book attribute

--- tests/ecore.tests/src/XmlResourceTests.cpp
+++ tests/ecore.tests/src/XmlResourceTests.cpp
@@ -58,9 +58,21 @@ BOOST_AUTO_TEST_CASE( Load )
     auto eBooksReference = std::dynamic_pointer_cast<EReference>( eBooksFeature );
     BOOST_CHECK( eBooksReference );
     
-
     auto eBook = eClassifiers->get( 1 );
     BOOST_CHECK_EQUAL( eBook->getName(), "Book" );
+    auto eBookClass = std::dynamic_pointer_cast<EClass>( eBook );
+    BOOST_CHECK( eBookClass );
+    BOOST_CHECK_EQUAL( eBookClass->getFeatureCount(), 2 );
+    
+    auto eNameFeature = eBookClass->getEStructuralFeature( 0 );
+    BOOST_CHECK_EQUAL( eNameFeature->getName(), "name" );
+    auto eNameAttribute = std::dynamic_pointer_cast<EAttribute>( eNameFeature );
+    BOOST_CHECK( eNameAttribute );
+   
+    auto eISBNFeature = eBookClass->getEStructuralFeature( 1 );
+    BOOST_CHECK_EQUAL( eISBNFeature->getName(), "isbn" );
+    auto eISBNAttribute = std::dynamic_pointer_cast<EAttribute>( eISBNFeature );
+    BOOST_CHECK( eISBNAttribute );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
