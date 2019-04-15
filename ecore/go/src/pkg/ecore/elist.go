2879d3d1c9cd985143c1ed00556865e8cc33293c
Xwilarg <xwilarg@yahoo.fr>
2019-04-15 12:03:18 +0200

    [Go] Add basic EList interface

--- ecore/go/src/pkg/ecore/elist.go
+++ ecore/go/src/pkg/ecore/elist.go
@@ -1,5 +1,34 @@
 package ecore
 
+// EList is the interface for dynamic containers
 type EList interface {
+	Add(interface{}) bool
 
+	AddAll(EList) bool
+
+	Insert(uint64, interface{}) bool
+
+	InsertAll(uint64, EList) bool
+
+	Move(uint64, interface{})
+
+	Order(uint64, uint64) interface{}
+
+	Get(uint64) interface{}
+
+	Set(uint64, interface{})
+
+	RemoveAt(uint64) interface{}
+
+	Remove(interface{}) bool
+
+	Size() uint64
+
+	Clear()
+
+	Empty()
+
+	Contains(interface{}) bool
+
+	IndexOf(interface{}) uint64
 }
