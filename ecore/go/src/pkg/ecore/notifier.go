453129233c5d24a2022990359fb1115fe42689cb
Xwilarg <xwilarg@yahoo.fr>
2019-06-03 12:01:00 +0200

    [Go] Fix Notifier case

--- ecore/go/src/pkg/ecore/notifier.go
+++ ecore/go/src/pkg/ecore/notifier.go
@@ -1,35 +1,35 @@
 package ecore
 
-type notifier struct {
+type Notifier struct {
 	eDeliver_  bool
 	eAdapters_ EList
 }
 
-func NewNotifier() *notifier {
-	return &notifier{
+func NewNotifier() *Notifier {
+	return &Notifier{
 		eDeliver_:  true,
 		eAdapters_: NewArrayEList([]interface{}{}),
 	}
 }
 
-func (notif *notifier) EAdapters() EList {
+func (notif *Notifier) EAdapters() EList {
 	return notif.eAdapters_
 }
 
-func (notif *notifier) EDeliver() bool {
+func (notif *Notifier) EDeliver() bool {
 	return notif.eDeliver_
 }
 
-func (notif *notifier) ESetDeliver(value bool) {
+func (notif *Notifier) ESetDeliver(value bool) {
 	notif.eDeliver_ = value
 }
 
-func (notif *notifier) ENotify(notification ENotification) {
+func (notif *Notifier) ENotify(notification ENotification) {
 	for it := notif.eAdapters_.Iterate(); it.Next(); {
 		it.Value().(EAdapter).NotifyChanged(notification)
 	}
 }
 
-func (notif *notifier) ENotificationRequired() bool {
+func (notif *Notifier) ENotificationRequired() bool {
 	return notif.eAdapters_ != nil && notif.eAdapters_.Size() > 0
 }
