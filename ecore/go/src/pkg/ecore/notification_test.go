765140c0cc1e0d0cb9269d950ee5b4c449249841
Xwilarg <xwilarg@yahoo.fr>
2019-06-04 10:09:19 +0200

    Fix notification tests

--- ecore/go/src/pkg/ecore/notification_test.go
+++ ecore/go/src/pkg/ecore/notification_test.go
@@ -9,21 +9,13 @@ type customTestAdapter struct {
 	Adapter
 }
 
-func (adapter customTestAdapter) SetTarget(notifier ENotifier) {
-	adapter.Adapter.SetTarget(notifier)
-}
-
-func (adapter customTestAdapter) GetTarget() ENotifier {
-	return adapter.Adapter.GetTarget()
-}
-
-func (adapter customTestAdapter) NotifyChanged(notification ENotification) {
+func (adapter *customTestAdapter) NotifyChanged(notification ENotification) {
 	fmt.Println("Notification received!")
 }
 
 func TestNotification(t *testing.T) {
 	testClassifier := NewEClassifierImpl()
-	adapter := customTestAdapter{Adapter: *NewAdapter()}
+	adapter := &customTestAdapter{Adapter: *NewAdapter()}
 	testClassifier.Notifier.EAdapters().Add(adapter)
 	testClassifier.SetClassifierID(0)
 }
