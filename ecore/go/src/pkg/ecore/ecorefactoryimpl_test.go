ce8a11e66f6b9cc878af5a1cf2fa54044c70fe96
Xwilarg <xwilarg@yahoo.fr>
2019-06-18 17:31:03 +0200

    Add factory tests

--- ecore/go/src/pkg/ecore/ecorefactoryimpl_test.go
+++ ecore/go/src/pkg/ecore/ecorefactoryimpl_test.go
@@ -1,1 +1,13 @@
 package ecore
+
+import "testing"
+
+func TestFactoryCreationFromString(t *testing.T) {
+	factory := NewEcoreFactoryExt()
+	check(t, factory.CreateEIntFromString(nil, "9"), int32(9), "Factory create variable from string")
+}
+
+func TestFactoryCreationEClass(t *testing.T) {
+	factory := NewEcoreFactoryExt()
+	checkNot(t, factory.CreateEClass(), nil, "Factory create EClass")
+}
