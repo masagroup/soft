8e3b556eb7cfb05babc6cb6c1fe87a14278c0172
Xwilarg <xwilarg@yahoo.fr>
2019-06-19 11:21:33 +0200

    [Gen][Go] Fix factory tests

--- ecore/go/src/pkg/ecore/ecorefactoryimpl_test.go
+++ ecore/go/src/pkg/ecore/ecorefactoryimpl_test.go
@@ -2,12 +2,9 @@ package ecore
 
 import "testing"
 
-func TestFactoryCreationFromString(t *testing.T) {
+func TestFactory(t *testing.T) {
 	factory := NewEcoreFactoryExt()
-	check(t, factory.CreateEIntFromString(nil, "9"), int32(9), "Factory create variable from string")
-}
-
-func TestFactoryCreationEClass(t *testing.T) {
-	factory := NewEcoreFactoryExt()
-	checkNot(t, factory.CreateEClass(), nil, "Factory create EClass")
+	eDataType := factory.CreateEDataTypeFromContainerAndClassID(nil, EINT)
+	checkNot(t, eDataType, nil, "Factory create EDataType")
+	check(t, factory.CreateFromString(eDataType, "9"), int32(9), "Factory create variable from string")
 }
