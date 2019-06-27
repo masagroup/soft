4fa085f53fff406a667efcf2c0250820ecaa6680
Xwilarg <xwilarg@yahoo.fr>
2019-06-27 17:53:49 +0200

    [Go] Improve Notifying List

--- ecore/go/pkg/ecore/enotifyinglist.go
+++ ecore/go/pkg/ecore/enotifyinglist.go
@@ -16,7 +16,7 @@ func NewENotifyingListImpl() *ENotifyingListImpl {
 
 // Add a new element to the array
 func (arr *ENotifyingListImpl) Add(elem interface{}) bool {
-	if arr.EList.Add(elem) {
+	if arr.Add(elem) {
 		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, elem, arr.Size()))
 		return true
 	}
@@ -25,10 +25,11 @@ func (arr *ENotifyingListImpl) Add(elem interface{}) bool {
 
 // AddAll elements of an array in the current one
 func (arr *ENotifyingListImpl) AddAll(list EList) bool {
-	if arr.EList.AddAll(list) {
-		if list.Size() == 1 {
+	if arr.AddAll(list) {
+		size := list.Size()
+		if size == 1 {
 			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, list, arr.Size()))
-		} else {
+		} else if size > 1 {
 			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, list, arr.Size()))
 		}
 		return true
@@ -38,7 +39,7 @@ func (arr *ENotifyingListImpl) AddAll(list EList) bool {
 
 // Insert an element in the array
 func (arr *ENotifyingListImpl) Insert(index int, elem interface{}) bool {
-	if arr.EList.Insert(index, elem) {
+	if arr.Insert(index, elem) {
 		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, elem, index))
 		return true
 	}
@@ -47,10 +48,11 @@ func (arr *ENotifyingListImpl) Insert(index int, elem interface{}) bool {
 
 // InsertAll element of an array at a given position
 func (arr *ENotifyingListImpl) InsertAll(index int, list EList) bool {
-	if arr.EList.InsertAll(index, list) {
-		if list.Size() == 1 {
+	if arr.InsertAll(index, list) {
+		size := list.Size()
+		if size == 1 {
 			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
-		} else {
+		} else if size > 1 {
 			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
 		}
 		return true
@@ -61,7 +63,7 @@ func (arr *ENotifyingListImpl) InsertAll(index int, list EList) bool {
 // RemoveAt remove an element at a given position
 func (arr *ENotifyingListImpl) RemoveAt(index int) bool {
 	old := arr.Get(index)
-	if arr.EList.RemoveAt(index) {
+	if arr.RemoveAt(index) {
 		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
 		return true
 	}
@@ -72,9 +74,27 @@ func (arr *ENotifyingListImpl) RemoveAt(index int) bool {
 func (arr *ENotifyingListImpl) Remove(elem interface{}) bool {
 	index := arr.IndexOf(elem)
 	old := arr.Get(index)
-	if arr.EList.Remove(elem) {
+	if arr.Remove(elem) {
 		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
 		return true
 	}
 	return false
+}
+
+// Clear remove all elements of the array
+func (arr *ENotifyingListImpl) Clear() {
+	size := arr.Size()
+	arr.Clear()
+	if size == 1 {
+		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, nil, NO_INDEX))
+	} else if size > 1 {
+		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, nil, NO_INDEX))
+	}
+}
+
+// Set an element of the array
+func (arr *ENotifyingListImpl) Set(index int, elem interface{}) {
+	old := arr.Get(index)
+	arr.Set(index, elem)
+	arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, old, elem, index))
 }
