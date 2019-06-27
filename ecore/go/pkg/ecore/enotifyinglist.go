5eaf878f43c87b7aa0abed14c57d644c1b57c1b1
Xwilarg <xwilarg@yahoo.fr>
2019-06-27 15:30:03 +0200

    [Go] Improve Notifying List

--- ecore/go/pkg/ecore/enotifyinglist.go
+++ ecore/go/pkg/ecore/enotifyinglist.go
@@ -35,3 +35,46 @@ func (arr *ENotifyingListImpl) AddAll(list EList) bool {
 	}
 	return false
 }
+
+// Insert an element in the array
+func (arr *ENotifyingListImpl) Insert(index int, elem interface{}) bool {
+	if arr.EList.Insert(index, elem) {
+		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, elem, index))
+		return true
+	}
+	return false
+}
+
+// InsertAll element of an array at a given position
+func (arr *ENotifyingListImpl) InsertAll(index int, list EList) bool {
+	if arr.EList.InsertAll(index, list) {
+		if list.Size() == 1 {
+			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
+		} else {
+			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
+		}
+		return true
+	}
+	return false
+}
+
+// RemoveAt remove an element at a given position
+func (arr *ENotifyingListImpl) RemoveAt(index int) bool {
+	old := arr.Get(index)
+	if arr.EList.RemoveAt(index) {
+		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
+		return true
+	}
+	return false
+}
+
+// Remove an element in an array
+func (arr *ENotifyingListImpl) Remove(elem interface{}) bool {
+	index := arr.IndexOf(elem)
+	old := arr.Get(index)
+	if arr.EList.Remove(elem) {
+		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
+		return true
+	}
+	return false
+}
