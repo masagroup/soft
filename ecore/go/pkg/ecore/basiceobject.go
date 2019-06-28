97a1a2a9de6156230a1afc322e9a585c17af8c4c
Jocelyn Dumay <jdy@masagroup.net>
2019-06-28 12:07:22 +0200

    [ecore][go] default implementation of EGet/ESet/EUnset/EIsSet methods

--- ecore/go/pkg/ecore/basiceobject.go
+++ ecore/go/pkg/ecore/basiceobject.go
@@ -2,6 +2,7 @@ package ecore
 
 import (
 	"net/url"
+	"strconv"
 )
 
 // BasicEObject is a basic implementation of an EObject
@@ -17,6 +18,11 @@ type BasicEObject struct {
 
 type EObjectInternal interface {
 	EStaticClass() EClass
+	EGetFromID(featureID int , resolve bool , core bool ) interface{}
+	ESetFromID( featureID int, newValue interface{})
+	EUnsetFromID(featureID int )
+	EIsSetFromID(featureID int ) bool
+	EInvokeFromID(operationID int, arguments EList) interface{}
 }
 
 // NewBasicEObject is BasicEObject constructor
@@ -96,26 +102,105 @@ func ( o *BasicEObject) ECrossReferences() EList {
 	return nil
 }
 
-func ( o *BasicEObject) EGet(EStructuralFeature) interface{} {
+func ( o *BasicEObject) eDerivedStructuralFeatureID( eFeature EStructuralFeature) int {
+	if !o.EClass().GetEAllStructuralFeatures().Contains( eFeature ) {
+		panic( "The feature '"+ eFeature.GetName() + "' is not a valid feature" )
+	} 
+    return eFeature.GetFeatureID()
+}
+
+func ( o *BasicEObject) eDerivedOperationID( eOperation EOperation ) int {
+	if !o.EClass().GetEAllOperations().Contains( eOperation ) {
+		panic( "The operation '"+ eOperation.GetName() + "' is not a valid operation" )
+	} 
+    return eOperation.GetOperationID()
+}
+
+func ( o *BasicEObject) EGet(eFeature EStructuralFeature) interface{} {
+	return o.eGetFromFeature( eFeature, true, true )
+}
+
+func ( o *BasicEObject) EGetResolve( eFeature EStructuralFeature, resolve bool) interface{} {
+	return o.eGetFromFeature( eFeature, resolve, true )
+}
+
+func ( o *BasicEObject) eGetFromFeature(eFeature EStructuralFeature , resolve bool , core bool ) interface{} {
+	featureID := o.eDerivedStructuralFeatureID( eFeature )
+    if featureID >= 0 {
+		return o.internal.(EObjectInternal).EGetFromID( featureID, resolve, core )
+	}
+    panic("The feature '" + eFeature.GetName() + "' is not a valid feature")
+}
+
+func ( o *BasicEObject) EGetFromID(featureID int , resolve bool , core bool ) interface{} {
+	eFeature := o.EClass().GetEStructuralFeature( featureID );
+	if eFeature == nil {
+		panic( "Invalid featureID: " + strconv.Itoa(featureID) )
+	}
 	return nil
 }
 
-func ( o *BasicEObject) EGetResolve(EStructuralFeature, bool) interface{} {
-	return nil
+func ( o *BasicEObject) ESet( eFeature EStructuralFeature, newValue interface{}) {
+	featureID := o.eDerivedStructuralFeatureID( eFeature );
+    if( featureID >= 0 ) {
+		o.internal.(EObjectInternal).ESetFromID( featureID, newValue );
+	} else {
+		panic("The feature '" + eFeature.GetName() + "' is not a valid feature")
+	}
 }
 
-func ( o *BasicEObject) ESet(EStructuralFeature, interface{}) {
+func ( o *BasicEObject) ESetFromID( featureID int, newValue interface{}) {
+	eFeature := o.EClass().GetEStructuralFeature( featureID );
+	if ( eFeature == nil ) {
+		panic( "Invalid featureID: " + strconv.Itoa(featureID) )
+	}
 }
 
-func ( o *BasicEObject) EIsSet(EStructuralFeature) bool {
-	return false
+func ( o *BasicEObject) EIsSet( eFeature EStructuralFeature) bool {
+	featureID := o.eDerivedStructuralFeatureID( eFeature )
+    if( featureID >= 0 ) {
+		return o.internal.(EObjectInternal).EIsSetFromID( featureID )
+	}
+	panic( "The feature '" + eFeature.GetName() + "' is not a valid feature" )
 }
 
-func ( o *BasicEObject) EUnset(EStructuralFeature)  {
-	
+func ( o *BasicEObject) EIsSetFromID(featureID int ) bool {
+	eFeature := o.EClass().GetEStructuralFeature( featureID )
+	if eFeature == nil {
+		panic( "Invalid featureID: " + strconv.Itoa(featureID) )
+	}
+    return false;
 }
 
-func ( o *BasicEObject) EInvoke(EOperation, EList) interface{}  {
+func ( o *BasicEObject) EUnset(eFeature EStructuralFeature)  {
+	featureID := o.eDerivedStructuralFeatureID( eFeature )
+    if( featureID >= 0 ) {
+		o.internal.(EObjectInternal).EUnsetFromID( featureID  )
+	} else {
+		panic("The feature '" + eFeature.GetName() + "' is not a valid feature")
+	}
+}
+
+func ( o *BasicEObject) EUnsetFromID(featureID int ) {
+	eFeature := o.EClass().GetEStructuralFeature( featureID )
+	if eFeature == nil {
+		panic( "Invalid featureID: " + strconv.Itoa(featureID) )
+	}
+}
+
+func ( o *BasicEObject) EInvoke(eOperation EOperation, arguments EList) interface{}  {
+	operationID := o.eDerivedOperationID( eOperation );
+    if( operationID >= 0 ) {
+		return o.internal.(EObjectInternal).EInvokeFromID( operationID, arguments )
+	}
+    panic("The operation '" + eOperation.GetName() + "' is not a valid operation");
+}
+
+func ( o *BasicEObject) EInvokeFromID(operationID int, arguments EList) interface{}  {
+	eOperation := o.EClass().GetEOperation( operationID )
+	if eOperation == nil {
+		panic( "Invalid operationID: " + strconv.Itoa(operationID) )
+	}
 	return nil
 }
 
