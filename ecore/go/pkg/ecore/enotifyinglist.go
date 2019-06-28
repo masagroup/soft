7d13a3699de15bccc3c00d5076cbb7ddcb455a05
Xwilarg <xwilarg@yahoo.fr>
2019-06-28 10:31:37 +0200

    [Go] Fix Notifying List notifications

--- ecore/go/pkg/ecore/enotifyinglist.go
+++ ecore/go/pkg/ecore/enotifyinglist.go
@@ -91,71 +91,9 @@ func (arr *ENotifyingListImpl) Set(index int, elem interface{}) {
 		if list.Size() == 1 {
 			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, list, arr.Size()))
 		} else if size > 1 {
-			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, list, arr.Size()))
+			arr.notifier.ENotify(NewNotificationByFeatureID(arr.owner, ADD_MANY, arr.featureID, nil, list, arr.Size()))
 		}
 		return true
 	}
 	return false
-}
-
-// Insert an element in the array
-func (arr *ENotifyingListImpl) Insert(index int, elem interface{}) bool {
-	if arr.Insert(index, elem) {
-		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, elem, index))
-		return true
-	}
-	return false
-}
-
-// InsertAll element of an array at a given position
-func (arr *ENotifyingListImpl) InsertAll(index int, list EList) bool {
-	if arr.InsertAll(index, list) {
-		size := list.Size()
-		if size == 1 {
-			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
-		} else if size > 1 {
-			arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, list, index))
-		}
-		return true
-	}
-	return false
-}
-
-// RemoveAt remove an element at a given position
-func (arr *ENotifyingListImpl) RemoveAt(index int) bool {
-	old := arr.Get(index)
-	if arr.RemoveAt(index) {
-		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
-		return true
-	}
-	return false
-}
-
-// Remove an element in an array
-func (arr *ENotifyingListImpl) Remove(elem interface{}) bool {
-	index := arr.IndexOf(elem)
-	old := arr.Get(index)
-	if arr.Remove(elem) {
-		arr.notifier.ENotify(NewNotificationByFeatureID(nil, REMOVE, ECLASSIFIER__INSTANCE_CLASS, old, nil, index))
-		return true
-	}
-	return false
-}
-
-// Clear remove all elements of the array
-func (arr *ENotifyingListImpl) Clear() {
-	size := arr.Size()
-	arr.Clear()
-	if size == 1 {
-		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD, ECLASSIFIER__INSTANCE_CLASS, nil, nil, NO_INDEX))
-	} else if size > 1 {
-		arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, nil, nil, NO_INDEX))
-	}
-}
-
-// Set an element of the array
-func (arr *ENotifyingListImpl) Set(index int, elem interface{}) {
-	old := arr.Get(index)
-	arr.Set(index, elem)
-	arr.notifier.ENotify(NewNotificationByFeatureID(nil, ADD_MANY, ECLASSIFIER__INSTANCE_CLASS, old, elem, index))
 }
