a18a14fe5e8e4729aa70cc95415d0ecdb63013cd
Jocelyn Dumay <jdy@masagroup.net>
2019-07-01 10:22:17 +0200

    [ecore][go] test EClass EAttributes / Features / References sub lists

--- ecore/go/pkg/ecore/eclass_test.go
+++ ecore/go/pkg/ecore/eclass_test.go
@@ -7,7 +7,24 @@ import (
 )
 
 func TestEClassInstance(t *testing.T) {
-	eClass := newEClassImpl()
+	eClass := newEClassExt()
 	eClass.SetName("eClass")
 	assert.Equal(t, eClass.GetName(), "eClass")
 }
+
+func TestEClassFeatures(t *testing.T) {
+	eClass := newEClassExt()
+	
+	eAttribute1 := newEAttributeExt()
+	eAttribute2 := newEAttributeExt()
+	eReference1 := newEReferenceExt()
+	eReference2 := newEReferenceExt()
+
+	eFeatures := eClass.GetEStructuralFeatures()
+	eFeatures.AddAll( NewImmutableEList([]interface{}{eAttribute1, eReference1, eAttribute2, eReference2 } ) )
+
+	assert.Equal(t, eClass.GetEAttributes().ToArray(), []interface{}{eAttribute1, eAttribute2})
+	assert.Equal(t, eClass.GetEReferences().ToArray(), []interface{}{eReference1, eReference2})
+
+}
+
