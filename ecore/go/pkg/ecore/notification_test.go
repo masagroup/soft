de40f58236a345ed95f15c0f9a2005a1f7d15d7f
Jocelyn Dumay <jdy@masagroup.net>
2019-07-02 17:15:40 +0200

    [ecore][test] test notification Constructor / GetFeature / GetFeatureID

--- ecore/go/pkg/ecore/notification_test.go
+++ ecore/go/pkg/ecore/notification_test.go
@@ -6,6 +6,80 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNotificationConstructor(t *testing.T) {
+	mockObject := &MockEObject{}
+	mockFeature := &MockEStructuralFeature{}
+	{
+		notification  := NewNotificationByFeature(mockObject, ADD, mockFeature, 1,2, NO_INDEX)
+		assert.Equal( t , notification.GetNotifier() , mockObject )
+		assert.Equal( t , notification.GetEventType() , ADD )
+		assert.Equal( t , notification.GetFeature() , mockFeature )
+		assert.Equal( t , notification.GetOldValue() , 1 )
+		assert.Equal( t , notification.GetNewValue() , 2 )
+		assert.Equal( t , notification.GetPosition() , NO_INDEX )
+	}
+	{
+		notification  := NewNotificationByFeature(mockObject, REMOVE, mockFeature, 1, 2, 3)
+		assert.Equal( t , notification.GetNotifier() , mockObject )
+		assert.Equal( t , notification.GetEventType() , REMOVE )
+		assert.Equal( t , notification.GetFeature() , mockFeature )
+		assert.Equal( t , notification.GetOldValue() , 1 )
+		assert.Equal( t , notification.GetNewValue() , 2 )
+		assert.Equal( t , notification.GetPosition() , 3 )
+	}
+	{
+		notification  := NewNotificationByFeatureID(mockObject, ADD_MANY, 10, 1, 2, NO_INDEX)
+		assert.Equal( t , notification.GetNotifier() , mockObject )
+		assert.Equal( t , notification.GetEventType() , ADD_MANY )
+		assert.Equal( t , notification.GetFeatureID() , 10 )
+		assert.Equal( t , notification.GetOldValue() , 1 )
+		assert.Equal( t , notification.GetNewValue() , 2 )
+		assert.Equal( t , notification.GetPosition() , NO_INDEX )
+	}
+	{
+		notification  := NewNotificationByFeatureID(mockObject, REMOVE_MANY, 10, 1, 2, 3)
+		assert.Equal( t , notification.GetNotifier() , mockObject )
+		assert.Equal( t , notification.GetEventType() , REMOVE_MANY )
+		assert.Equal( t , notification.GetFeatureID() , 10 )
+		assert.Equal( t , notification.GetOldValue() , 1 )
+		assert.Equal( t , notification.GetNewValue() , 2 )
+		assert.Equal( t , notification.GetPosition() , 3 )
+	}
+}
+
+func TestNotificationGetFeatureID(t *testing.T) {
+	mockObject := &MockEObject{}
+	mockFeature := &MockEStructuralFeature{}
+	{
+		notification  := NewNotificationByFeatureID(mockObject, REMOVE, 10, 1, 2, 3)
+		assert.Equal( t , notification.GetFeatureID() , 10 )
+	}
+	{
+		notification  := NewNotificationByFeature(mockObject, ADD, mockFeature, 1,2, NO_INDEX)
+		mockFeature.On("GetFeatureID").Return(5)
+		assert.Equal( t , notification.GetFeatureID() , 5 )
+	}
+	
+
+}
+
+func TestNotificationGetFeature(t *testing.T) {
+	mockObject := &MockEObject{}
+	mockFeature := &MockEStructuralFeature{}
+	mockClass := &MockEClass{}
+	{
+		notification  := NewNotificationByFeature(mockObject, ADD_MANY, mockFeature, 1, 2, NO_INDEX)
+		assert.Equal( t , notification.GetFeature() , mockFeature )
+	}
+	{
+		notification  := NewNotificationByFeatureID(mockObject, REMOVE_MANY, 10, 1, 2, 3)
+		mockObject.On("EClass").Return(mockClass)
+		mockClass.On("GetEStructuralFeature",10).Return(mockFeature)
+		assert.Equal( t , notification.GetFeature() , mockFeature )
+	}
+}
+
+
 type customTestAdapterMerge struct {
 	Adapter
 }
