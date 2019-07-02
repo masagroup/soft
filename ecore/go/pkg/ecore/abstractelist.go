69b10df5f3469c110c35d887faba7fba128eabbb
Xwilarg <xwilarg@yahoo.fr>
2019-07-02 17:57:48 +0200

    [Go] Add do functions for Array List

--- ecore/go/pkg/ecore/abstractelist.go
+++ ecore/go/pkg/ecore/abstractelist.go
@@ -1,4 +1,13 @@
 package ecore
 
 type abstractEList interface {
+	doAdd(elem interface{})
+
+	doAddAll(list EList)
+
+	doInsert(index int, elem interface{})
+
+	doInsertAll(index int, list EList)
+
+	doSet(index int, elem interface{})
 }
