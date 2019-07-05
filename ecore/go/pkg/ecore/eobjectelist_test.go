fc0273af33d07dd89397841c1ba1b199d8655a81
Xwilarg <xwilarg@yahoo.fr>
2019-07-05 17:44:04 +0200

    [Go] Add EObject EList tests

--- ecore/go/pkg/ecore/eobjectelist_test.go
+++ ecore/go/pkg/ecore/eobjectelist_test.go
@@ -2,36 +2,65 @@ package ecore
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	mock "github.com/stretchr/testify/mock"
 )
 
-type testingObjectEList struct {
+type eObjectEListExample struct {
 	*eObjectEList
+	c bool
+	i bool
+	o bool
+	p bool
+	u bool
 }
 
-func (arr *testingObjectEList) containment() bool {
-	return true
+func (e *eObjectEListExample) containment() bool {
+	return e.c
 }
 
-func (arr *testingObjectEList) inverse() bool {
-	return false
+func (e *eObjectEListExample) inverse() bool {
+	return e.i
 }
 
-func (arr *testingObjectEList) opposite() bool {
-	return false
+func (e *eObjectEListExample) opposite() bool {
+	return e.o
 }
 
-func (arr *testingObjectEList) proxies() bool {
-	return false
+func (e *eObjectEListExample) proxies() bool {
+	return e.p
 }
 
-func (arr *testingObjectEList) unset() bool {
-	return false
+func (e *eObjectEListExample) unset() bool {
+	return e.u
+}
+
+func newEObjectEListExample(owner EObjectInternal, id, reverseID int, c, i, o, u, p bool) *eObjectEListExample {
+	tmp := &eObjectEListExample{
+		eObjectEList: NewEObjectEList(owner, 1, 2),
+	}
+	tmp.internal = tmp
+	return tmp
 }
 
 func TestEObjectEList(t *testing.T) {
-	arr := &testingObjectEList{}
-	arr.eObjectEList = NewEObjectEList(NewBasicEObject(), -1, -1)
-
-	adapter := &customTestAdapterMerge{Adapter: *NewAdapter()}
-	arr.owner.EAdapters().Add(adapter)
+	mockOwner := &MockEObjectInternal{}
+	mockObject := &MockEObject{}
+	mockOwner.On("ENotify", mock.Anything).Return()
+	{
+		list := newEObjectEListExample(mockOwner, 1, 2, false, false, false, false, false)
+		assert.True(t, list.Add(mockObject))
+		assert.Equal(t, list.Size(), 1)
+		assert.Equal(t, list.Get(0), mockObject)
+		assert.True(t, !list.Add(mockObject))
+	}
+	{
+		mockOwner.On("EInverseAdd", mockOwner, 2, nil).Once().Return(nil)
+		list := newEObjectEListExample(mockOwner, 1, 2, false, true, true, false, false)
+		assert.True(t, list.Add(mockObject))
+		assert.Equal(t, list.Size(), 1)
+		assert.Equal(t, list.Get(0), mockObject)
+		assert.True(t, !list.Add(mockObject))
+	}
 }
