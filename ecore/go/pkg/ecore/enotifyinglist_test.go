45d5637f4fd82a912dca0521ba31002dedc95f03
Xwilarg <xwilarg@yahoo.fr>
2019-07-09 16:46:38 +0200

    [Go] Replace Notifying tests using mocking

--- ecore/go/pkg/ecore/enotifyinglist_test.go
+++ ecore/go/pkg/ecore/enotifyinglist_test.go
@@ -3,53 +3,17 @@ package ecore
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	mock "github.com/stretchr/testify/mock"
 )
 
-type customTestAdapterMerge struct {
-	Adapter
-}
-
-var notif = NewArrayEList([]interface{}{})
-
-func (adapter *customTestAdapterMerge) NotifyChanged(notification ENotification) {
-	notif.Add(notification)
-}
-
-func TestNotificationListAdd(t *testing.T) {
-	arr := NewENotifyingListImpl(NewBasicEObject(), -1)
-	notif = NewArrayEList([]interface{}{})
-
-	adapter := &customTestAdapterMerge{Adapter: *NewAdapter()}
-	arr.owner.EAdapters().Add(adapter)
-	arr.Add(3)
-	assert.Equal(t, notif.Size(), 1, "Notification count")
-	assert.Equal(t, notif.Get(0).(ENotification).GetNewValue(), 3)
-	assert.Equal(t, notif.Get(0).(ENotification).GetEventType(), ADD)
-}
-
-func TestNotificationListAddAll(t *testing.T) {
-	arr := NewENotifyingListImpl(NewBasicEObject(), -1)
-	arr2 := NewArrayEList([]interface{}{3, 2})
-	notif = NewArrayEList([]interface{}{})
-
-	adapter := &customTestAdapterMerge{Adapter: *NewAdapter()}
-	arr.owner.EAdapters().Add(adapter)
-	arr.AddAll(arr2)
-	assert.Equal(t, notif.Size(), 1, "Notification count")
-	assert.Equal(t, notif.Get(0).(ENotification).GetNewValue(), []interface{}{3, 2})
-	assert.Equal(t, notif.Get(0).(ENotification).GetEventType(), ADD_MANY)
-}
-
-func TestNotificationListAddAllOne(t *testing.T) {
-	arr := NewENotifyingListImpl(NewBasicEObject(), -1)
-	arr2 := NewArrayEList([]interface{}{2, 2})
-	notif = NewArrayEList([]interface{}{})
-
-	adapter := &customTestAdapterMerge{Adapter: *NewAdapter()}
-	arr.owner.EAdapters().Add(adapter)
-	arr.AddAll(arr2)
-	assert.Equal(t, notif.Size(), 1, "Notification count")
-	assert.Equal(t, notif.Get(0).(ENotification).GetNewValue(), 2)
-	assert.Equal(t, notif.Get(0).(ENotification).GetEventType(), ADD)
+func TestENotifyingList(t *testing.T) {
+	mockOwner := &MockEObjectInternal{}
+	{
+		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
+			return notif.GetNewValue() == 3 &&
+				notif.GetEventType() == ADD
+		})).Return()
+		arr := NewENotifyingListImpl(mockOwner, -1)
+		arr.Add(3)
+	}
 }
