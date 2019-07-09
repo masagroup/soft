3991282dbefac59ea3ee38e0986e9ddd836bcfdc
Xwilarg <xwilarg@yahoo.fr>
2019-07-09 17:32:58 +0200

    [Go] Improve EObjectEList unit tests

--- ecore/go/pkg/ecore/enotifyinglist_test.go
+++ ecore/go/pkg/ecore/enotifyinglist_test.go
@@ -1,6 +1,7 @@
 package ecore
 
 import (
+	"reflect"
 	"testing"
 
 	mock "github.com/stretchr/testify/mock"
@@ -11,9 +12,46 @@ func TestENotifyingList(t *testing.T) {
 	{
 		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
 			return notif.GetNewValue() == 3 &&
+				notif.GetOldValue() == nil &&
 				notif.GetEventType() == ADD
 		})).Return()
 		arr := NewENotifyingListImpl(mockOwner, -1)
 		arr.Add(3)
+	}
+	{
+		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
+			return notif.GetNewValue() == 1 &&
+				notif.GetOldValue() == nil &&
+				notif.GetEventType() == ADD
+		})).Return()
+		arr := NewENotifyingListImpl(mockOwner, -1)
+		arr.AddAll(NewArrayEList([]interface{}{1}))
+	}
+	{
+		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
+			return reflect.DeepEqual(notif.GetNewValue(), []interface{}{1, 2, 3}) &&
+				notif.GetOldValue() == nil &&
+				notif.GetEventType() == ADD_MANY
+		})).Return()
+		arr := NewENotifyingListImpl(mockOwner, -1)
+		arr.AddAll(NewArrayEList([]interface{}{1, 2, 3}))
+	}
+	{
+		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
+			return notif.GetNewValue() == 3 &&
+				notif.GetOldValue() == nil &&
+				notif.GetEventType() == ADD
+		})).Return()
+		arr := NewENotifyingListImpl(mockOwner, -1)
+		arr.Insert(0, 3)
+	}
+	{
+		mockOwner.On("ENotify", mock.MatchedBy(func(notif *notification) bool {
+			return reflect.DeepEqual(notif.GetNewValue(), []interface{}{1, 2, 3}) &&
+				notif.GetOldValue() == nil &&
+				notif.GetEventType() == ADD_MANY
+		})).Return()
+		arr := NewENotifyingListImpl(mockOwner, -1)
+		arr.InsertAll(0, NewArrayEList([]interface{}{1, 2, 3}))
 	}
 }
