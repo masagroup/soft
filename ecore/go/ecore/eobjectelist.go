ea424fb1076c8842a9b16bdd36bdcf032368134f
Jocelyn Dumay <jdy@masagroup.net>
2019-07-15 11:38:00 +0200

    [ecore][go] define inversAdd / inversRemove methods in EObjectEList

--- ecore/go/ecore/eobjectelist.go
+++ ecore/go/ecore/eobjectelist.go
@@ -13,141 +13,56 @@ type eObjectEList struct {
 }
 
 func NewEObjectEList(owner EObjectInternal, featureID int, inverseFeatureID int, containment, inverse, opposite, proxies, unset bool) *eObjectEList {
-	list := &eObjectEList{
-		owner:              owner,
-		featureID:          featureID,
-		inverseFeatureID:   inverseFeatureID,
-		ENotifyingListImpl: NewENotifyingListImpl(),
-		containment:        containment,
-		inverse:            inverse,
-		opposite:           opposite,
-		proxies:            proxies,
-		unset:              unset,
-	}
-	return list
+	l := new(eObjectEList)
+	l.ENotifyingListImpl = NewENotifyingListImpl()
+	l.interfaces = l
+	l.owner = owner
+	l.featureID = featureID
+	l.inverseFeatureID = inverseFeatureID
+	l.containment = containment
+	l.inverse = inverse
+	l.opposite = opposite
+	l.proxies = proxies
+	l.unset = unset
+	return l
 }
 
-func (arr *eObjectEList) Add(elem interface{}) bool {
-	ret := arr.ENotifyingListImpl.Add(elem)
-	if arr.inverse {
-		var notifications ENotificationChain
-		if arr.opposite {
-			notifications = arr.owner.EInverseAdd(elem.(EObject), arr.inverseFeatureID, nil)
+// GetNotifier ...
+func (list *eObjectEList) GetNotifier() ENotifier {
+	return list.owner
+}
+	
+// GetFeature ...
+func (list *eObjectEList) GetFeature() EStructuralFeature {
+	if ( list.owner != nil ) {
+		return list.owner.EClass().GetEStructuralFeature(list.featureID)
+	}
+	return nil
+}
+	
+// GetFeatureID ...
+func (list *eObjectEList) GetFeatureID() int {
+	return list.featureID
+}
+
+func (list *eObjectEList) inverseAdd( object interface{}, notifications ENotificationChain ) ENotificationChain {
+	if ( object != nil && list.inverse ) {
+		if ( list.opposite ) {
+			return object.(EObjectInternal).EInverseAdd( list.interfaces.(EObject) , list.inverseFeatureID , notifications )
 		} else {
-			notifications = arr.owner.EInverseAdd(elem.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, nil)
+			return object.(EObjectInternal).EInverseAdd( list.interfaces.(EObject) , EOPPOSITE_FEATURE_BASE - list.featureID , notifications)
 		}
-		notifications.Dispatch()
-	}
-	return ret
+	} 
+	return notifications
 }
 
-func (arr *eObjectEList) AddAll(list EList) bool {
-	ret := arr.ENotifyingListImpl.AddAll(list)
-	if arr.inverse {
-		var notifications ENotificationChain
-		notifications = NewNotificationChain()
-		if arr.opposite {
-			for it := list.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseAdd(it.Value().(EObject), arr.inverseFeatureID, notifications)
-			}
+func (list *eObjectEList) inverseRemove( object interface{}, notifications ENotificationChain ) ENotificationChain {
+	if ( object != nil && list.inverse ) {
+		if ( list.opposite ) {
+			return object.(EObjectInternal).EInverseRemove( list.interfaces.(EObject) , list.inverseFeatureID , notifications)
 		} else {
-			for it := list.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseAdd(it.Value().(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, notifications)
-			}
+			return object.(EObjectInternal).EInverseRemove( list.interfaces.(EObject) , EOPPOSITE_FEATURE_BASE - list.featureID , notifications)
 		}
-		notifications.Dispatch()
-	}
-	return ret
-}
-
-func (arr *eObjectEList) Insert(index int, elem interface{}) bool {
-	ret := arr.ENotifyingListImpl.Insert(index, elem)
-	if arr.inverse {
-		var notifications ENotificationChain
-		if arr.opposite {
-			notifications = arr.owner.EInverseAdd(elem.(EObject), arr.inverseFeatureID, nil)
-		} else {
-			notifications = arr.owner.EInverseAdd(elem.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, nil)
-		}
-		notifications.Dispatch()
-	}
-	return ret
-}
-
-func (arr *eObjectEList) InsertAll(index int, list EList) bool {
-	ret := arr.ENotifyingListImpl.InsertAll(index, list)
-	if arr.inverse {
-		var notifications ENotificationChain
-		notifications = NewNotificationChain()
-		if arr.opposite {
-			for it := list.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseAdd(it.Value().(EObject), arr.inverseFeatureID, notifications)
-			}
-		} else {
-			for it := list.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseAdd(it.Value().(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, notifications)
-			}
-		}
-		notifications.Dispatch()
-	}
-	return ret
-}
-
-func (arr *eObjectEList) Remove(elem interface{}) bool {
-	ret := arr.ENotifyingListImpl.Remove(elem)
-	if arr.inverse {
-		var notifications ENotificationChain
-		if arr.opposite {
-			notifications = arr.owner.EInverseRemove(elem.(EObject), arr.inverseFeatureID, nil)
-		} else {
-			notifications = arr.owner.EInverseRemove(elem.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, nil)
-		}
-		notifications.Dispatch()
-	}
-	return ret
-}
-
-func (arr *eObjectEList) RemoveAt(index int) interface{} {
-	ret := arr.ENotifyingListImpl.RemoveAt(index)
-	if arr.inverse {
-		var notifications ENotificationChain
-		if arr.opposite {
-			notifications = arr.owner.EInverseRemove(ret.(EObject), arr.inverseFeatureID, nil)
-		} else {
-			notifications = arr.owner.EInverseRemove(ret.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, nil)
-		}
-		notifications.Dispatch()
-	}
-	return ret
-}
-
-func (arr *eObjectEList) Clear() {
-	old := arr
-	arr.ENotifyingListImpl.Clear()
-	if arr.inverse {
-		var notifications ENotificationChain
-		if arr.opposite {
-			for it := old.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseRemove(it.Value().(EObject), arr.inverseFeatureID, notifications)
-			}
-		} else {
-			for it := old.Iterate(); it.Next(); {
-				notifications = arr.owner.EInverseRemove(it.Value().(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, notifications)
-			}
-		}
-		notifications.Dispatch()
-	}
-}
-
-func (arr *eObjectEList) Set(index int, elem interface{}) {
-	old := arr.Get(index)
-	arr.ENotifyingListImpl.Set(index, elem)
-	var notifications ENotificationChain
-	if arr.opposite {
-		notifications = arr.owner.EInverseRemove(old.(EObject), arr.inverseFeatureID, notifications)
-		notifications = arr.owner.EInverseAdd(elem.(EObject), arr.inverseFeatureID, notifications)
-	} else {
-		notifications = arr.owner.EInverseRemove(old.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, notifications)
-		notifications = arr.owner.EInverseAdd(elem.(EObject), EOPPOSITE_FEATURE_BASE-arr.featureID, notifications)
-	}
+	} 
+	return notifications
 }
