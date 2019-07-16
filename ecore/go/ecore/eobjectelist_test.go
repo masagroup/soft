43a4b07e2ad3b8044080d9b538c0c65cf3974d68
Jocelyn Dumay <jdy@masagroup.net>
2019-07-16 11:03:15 +0200

    [ecore][go] add test for eobjectelist accessors

--- ecore/go/ecore/eobjectelist_test.go
+++ ecore/go/ecore/eobjectelist_test.go
@@ -7,6 +7,30 @@ import (
 	
 )
 
+func TestEObjectEListAccessors(t *testing.T) {
+	{
+		list := NewEObjectEList(nil, 1, -1, false, true, false, false, false)
+		assert.Equal(t , nil , list.GetNotifier() )
+		assert.Equal(t , nil , list.GetFeature() )
+		assert.Equal(t , 1 , list.GetFeatureID() )
+	}
+	{
+		mockOwner := &MockEObjectInternal{}
+		list := NewEObjectEList(mockOwner, 1, -1, false, true, false, false, false)
+		assert.Equal(t , mockOwner , list.GetNotifier() )
+		assert.Equal(t , 1 , list.GetFeatureID() )
+		mockClass := &MockEClass{}
+		mockFeature := &MockEStructuralFeature{}
+		mockClass.On("GetEStructuralFeature",1).Return(mockFeature)
+		mockOwner.On("EClass").Return(mockClass)
+		assert.Equal(t , mockFeature , list.GetFeature() )
+		mockOwner.AssertExpectations(t)
+		mockClass.AssertExpectations(t)
+		mockFeature.AssertExpectations(t)
+		mockClass.AssertExpectations(t)
+	}	
+}
+
 func TestEObjectEListInverseNoOpposite(t *testing.T) {
 	mockOwner := &MockEObjectInternal{}
 	mockOwner.On("EDeliver").Return( false )
