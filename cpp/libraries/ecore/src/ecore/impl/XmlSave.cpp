86d10fbf11b0055d3a144b769a2b003979b87f2e
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2019-11-29 15:21:34 +0100

    [ecore][cpp] define all methods in XmlSave class

--- cpp/libraries/ecore/src/ecore/impl/XmlSave.cpp
+++ cpp/libraries/ecore/src/ecore/impl/XmlSave.cpp
@@ -11,3 +11,153 @@ XmlSave::XmlSave(XmlResource& resource)
 XmlSave::~XmlSave()
 {
 }
+
+void XmlSave::save(std::ostream& o)
+{
+}
+
+void XmlSave::saveHeader()
+{
+}
+
+std::shared_ptr<XmlString::Segment> XmlSave::saveTopObject(const std::shared_ptr<EObject>& eObject)
+{
+    return std::shared_ptr<XmlString::Segment>();
+}
+
+void XmlSave::saveNamespaces()
+{
+}
+
+void XmlSave::saveElementID(const std::shared_ptr<EObject>& eObject)
+{
+}
+
+bool XmlSave::saveFeatures(const std::shared_ptr<EObject>& eObject, bool attributesOnly)
+{
+    return false;
+}
+
+void XmlSave::saveDataTypeSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveDataTypeMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveManyEmpty(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveEObjectSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveEObjectMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveNil(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveContainedSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveContainedMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveEObjectInternal(const std::shared_ptr<EObjectInternal>& eObjectInternal, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveEObject(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveTypeAttribute(const std::shared_ptr<EClass>& eClass)
+{
+}
+
+void XmlSave::saveHRefSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveHRefMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveHRef(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveIDRefSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+void XmlSave::saveIDRefMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+}
+
+bool XmlSave::isNil(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+    return false;
+}
+
+bool XmlSave::isEmpty(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+    return false;
+}
+
+bool XmlSave::shouldSaveFeature(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+    return false;
+}
+
+XmlSave::FeatureKind XmlSave::getFeatureKind(const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+    return FeatureKind();
+}
+
+XmlSave::ResourceKind XmlSave::getResourceKind(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature)
+{
+    return ResourceKind();
+}
+
+std::string XmlSave::getQName(const std::shared_ptr<EClass>& eClass)
+{
+    return std::string();
+}
+
+std::string XmlSave::getPrefix(const std::shared_ptr<EPackage>& ePackage, bool mustHavePrefix)
+{
+    return std::string();
+}
+
+std::string XmlSave::getDataType(const Any& value, const std::shared_ptr<EStructuralFeature>& eFeature, bool isAttribute)
+{
+    return std::string();
+}
+
+std::string XmlSave::getHRef(const std::shared_ptr<EObject>& eObject)
+{
+    return std::string();
+}
+
+std::string XmlSave::getHRef(const std::shared_ptr<EResource>& eResource, const std::shared_ptr<EObject>& eObject)
+{
+    return std::string();
+}
+
+std::string XmlSave::getIDRef(const std::shared_ptr<EObject>& eObject)
+{
+    return std::string();
+}
+
+std::string XmlSave::getQName(const std::shared_ptr<EPackage>& ePackage, const std::string& name, bool mustHavePrefix)
+{
+    return std::string();
+}
--- cpp/libraries/ecore/src/ecore/impl/XmlSave.hpp
+++ cpp/libraries/ecore/src/ecore/impl/XmlSave.hpp
@@ -10,10 +10,22 @@
 #ifndef ECORE_XMLSAVE_HPP_
 #define ECORE_XMLSAVE_HPP_
 
+#include "ecore/Any.hpp"
 #include "ecore/impl/XmlNamespaces.hpp"
+#include "ecore/impl/XmlString.hpp"
+
+namespace ecore {
+    class EClass;
+    class EObject;
+    class EPackage;
+    class EResource;
+    class EStructuralFeature;
+}
 
 namespace ecore::impl
 {
+    class EObjectInternal;
+
     class XmlResource;
 
     class XmlSave
@@ -23,9 +35,97 @@ namespace ecore::impl
 
         virtual ~XmlSave();
 
+        void save(std::ostream& o);
+
+    private:
+        void saveHeader();
+        std::shared_ptr<XmlString::Segment> saveTopObject(const std::shared_ptr<EObject>& eObject);
+        void saveNamespaces();
+        void saveElementID(const std::shared_ptr<EObject>& eObject);
+        bool saveFeatures(const std::shared_ptr<EObject>& eObject, bool attributesOnly);
+
+        void saveDataTypeSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveDataTypeMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveManyEmpty(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveEObjectSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveEObjectMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveNil(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveContainedSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveContainedMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveEObjectInternal(const std::shared_ptr<EObjectInternal>& eObjectInternal, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveEObject(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveTypeAttribute(const std::shared_ptr<EClass>& eClass);
+
+        void saveHRefSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveHRefMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveHRef(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        void saveIDRefSingle(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        void saveIDRefMany(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        bool isNil(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        bool isEmpty(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+        bool shouldSaveFeature(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        enum FeatureKind {
+            TRANSIENT,
+            DATATYPE_SINGLE,
+            DATATYPE_ELEMENT_SINGLE,
+            DATATYPE_CONTENT_SINGLE,
+            DATATYPE_SINGLE_NILLABLE,
+            DATATYPE_MANY,
+            OBJECT_CONTAIN_SINGLE,
+            OBJECT_CONTAIN_MANY,
+            OBJECT_HREF_SINGLE,
+            OBJECT_HREF_MANY,
+            OBJECT_CONTAIN_SINGLE_UNSETTABLE,
+            OBJECT_CONTAIN_MANY_UNSETTABLE,
+            OBJECT_HREF_SINGLE_UNSETTABLE,
+            OBJECT_HREF_MANY_UNSETTABLE,
+            OBJECT_ELEMENT_SINGLE,
+            OBJECT_ELEMENT_SINGLE_UNSETTABLE,
+            OBJECT_ELEMENT_MANY,
+            OBJECT_ELEMENT_IDREF_SINGLE,
+            OBJECT_ELEMENT_IDREF_SINGLE_UNSETTABLE,
+            OBJECT_ELEMENT_IDREF_MANY,
+            ATTRIBUTE_FEATURE_MAP,
+            ELEMENT_FEATURE_MAP,
+            OBJECT_ATTRIBUTE_SINGLE,
+            OBJECT_ATTRIBUTE_MANY,
+            OBJECT_ATTRIBUTE_IDREF_SINGLE,
+            OBJECT_ATTRIBUTE_IDREF_MANY,
+            DATATYPE_ATTRIBUTE_MANY,
+        };
+        
+        FeatureKind getFeatureKind(const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        enum ResourceKind {
+            SKIP,
+            SAME,
+            CROSS,
+        };
+
+        ResourceKind getResourceKind(const std::shared_ptr<EObject>& eObject, const std::shared_ptr<EStructuralFeature>& eFeature);
+
+        std::string getQName(const std::shared_ptr<EClass>& eClass);
+        std::string getQName(const std::shared_ptr<EPackage>& ePackage , const std::string& name, bool mustHavePrefix);
+        std::string getQName(const std::shared_ptr<EStructuralFeature>& eFeature);
+        std::string getPrefix(const std::shared_ptr<EPackage>& ePackage, bool mustHavePrefix);
+        std::string getDataType(const Any& value, const std::shared_ptr<EStructuralFeature>& eFeature, bool isAttribute);
+        std::string getHRef(const std::shared_ptr<EObject>& eObject);
+        std::string getHRef(const std::shared_ptr<EResource>& eResource, const std::shared_ptr<EObject>& eObject);
+        std::string getIDRef(const std::shared_ptr<EObject>& eObject);
+
     private:
         XmlResource& resource_;
         XmlNamespaces namespaces_;
+        XmlString str_;
     };
 }
 
