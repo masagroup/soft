558e63df2863fcf3e3ff9bda5bf420006bd02ae3
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-03-13 11:41:18 +0100

    [ecore][cpp] tests derived_pointer_cast function

---
+++ cpp/ecore/tests/src/SmartPtrTests.cpp
@@ -0,0 +1,63 @@
+#include <boost/test/unit_test.hpp>
+
+#include "ecore\SmartPtr.hpp"
+
+using namespace ecore;
+
+namespace
+{
+    class A
+    {
+    public:
+        virtual ~A() = default;
+    };
+
+    class B : public A
+    {
+    public:
+        virtual ~B() = default;
+    };
+
+    class C : public virtual A
+    {
+    public:
+        virtual ~C() = default;
+    };
+
+} // namespace
+
+BOOST_AUTO_TEST_SUITE( SmartPtrTests )
+
+BOOST_AUTO_TEST_CASE( DerivedPointerCast_Static )
+{
+    auto b = std::make_shared<B>();
+    auto a = std::static_pointer_cast<A>( b );
+    
+    auto b1 = derived_pointer_cast<B, B>( b );
+    BOOST_CHECK_EQUAL( b, b1 );
+
+    auto b2 = derived_pointer_cast<A, B>( a );
+    BOOST_CHECK_EQUAL( b, b2 );
+
+    auto a2 = derived_pointer_cast<B, A>( b );
+    BOOST_CHECK_EQUAL( a, a2 );
+}
+
+BOOST_AUTO_TEST_CASE( DerivedPointerCast_Dynamic )
+{
+    auto c = std::make_shared<C>();
+    auto a = std::static_pointer_cast<A>( c );
+
+    auto c1 = derived_pointer_cast<C, C>( c );
+    BOOST_CHECK_EQUAL( c, c1 );
+
+    auto c2 = derived_pointer_cast<A, C>( a );
+    BOOST_CHECK_EQUAL( c, c2 );
+
+    auto a2 = derived_pointer_cast<C, A>( c );
+    BOOST_CHECK_EQUAL( a, a2 );
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
